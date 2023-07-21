29ebcbd5409e1f0fe1b46f0dd8bc23fc589dc451
xiatian <2813576925@qq.com>
2023-07-21 10:13:43 +0800

    0.1.6二维柏林噪声

---
+++ 0.1.6二维柏林噪声/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(600, 400);
+  // background('DodgerBlue');
+}
+
+function draw() {
+  let x = 0;
+  loadPixels();
+  for(let i = 0;i < width;i ++){
+    x += 0.05;
+    let y = 3;
+    for(let j = 0;j < height;j ++){
+      y += 0.05;
+      let c = noise(x,y);
+      c = map(c,0,1,0,255);
+      set(i,j,color(c,0,c,255));
+    }
+  }
+
+  updatePixels()
+}
